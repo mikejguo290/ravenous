32e65ba4b1d1197d546736508cf823461e8434ff
mikejguo290 <mikejguo290@gmail.com>
2021-06-21 00:23:19 +0100

    26-27. Add SearchBar folder and with .css and .js files. save two background images into folder.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,3 +3,20 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 // render a list of returned Businesses from calling on YELP API
+
+class BusinessList extends React.Component {
+    render() {
+        return (
+            <div class="BusinessList">
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div>
+        )
+    }
+}
+
+export default BusinessList;
