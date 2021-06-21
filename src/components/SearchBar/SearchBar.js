5ff665e890c0ab44a37630789548ba8124b41d15
mikejguo290 <mikejguo290@gmail.com>
2021-06-21 13:20:19 +0100

    create renderSortByOptions method to return list of sort options with keys set to API sortBy parameter values.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,8 +6,23 @@ import './SearchBar.css';
 
 const sortByOptions = {
     // save list of key value pairs to format API GET requests later on. 
+    // keys = human readable text
+    // values = api sortBy parameter values
     'Best Match': 'best_match',
     'Highest Rated': 'rating',
     'Most Reviewed': 'review_count',
 }
 
+class SearchBar extends React.component {
+    
+    renderSortByOptions() {
+        // dynamically display the list used to show the three sort options
+        return (
+            // more semantic to use singular form of the thing you are applying .map() to instead of key, val, all the way. Although harder to read without creating intermediate variables.
+            Object.keys(sortByOptions).map((sortByOption)=> {
+                const sortByOptionValue = sortByOptions[sortByOption]; // confusiongly, sortByOptionValue is used for the key of the list item. 
+            <li key={sortByOptionValue}>{sortByOption}</li>
+            })
+        )
+    }
+}
