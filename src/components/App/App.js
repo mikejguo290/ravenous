8c4d8f336ee5cb2063182a882ecb5717991e23aa
mikejguo290 <mikejguo290@gmail.com>
2021-06-21 13:53:08 +0100

    45-48. import SearchBar, BusinessList return JSX based on template HTML and render two components. Export App.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './App.css'
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+class App extends React.component {
+  render(){
+    return (
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>    
+    )
+  }
+}
+
+export default App;
