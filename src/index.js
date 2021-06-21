1cb176eccf84315343ccef937db7e0b622eca70a
mikejguo290 <mikejguo290@gmail.com>
2021-06-21 17:09:33 +0100

    import registerServiceWorker().

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import registerServiceWorker from './registerServiceWorker.js'
 
 ReactDOM.render(
   <React.StrictMode>
