d4cb1aca3a821ebb2391e0a2292cb471a8808167
mikejguo290 <mikejguo290@gmail.com>
2021-06-21 18:59:36 +0100

    2.5 pass props businesses to BusinessList inside App's render method.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
       <div class="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>    
     )
   }
