0287ec752764035a0d960bc8d689aca5d75b617f
mikejguo290 <mikejguo290@gmail.com>
2021-06-24 22:53:17 +0100

    3.4.1 change references to sortByOptions to refer to member variable. Delete original local variable.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,15 +4,6 @@ import './SearchBar.css';
 //The search bar will communicate with the Yelp API. it will format the requests sent to the api 
 // and allow users to search by best match, highest rated and most reviewed.
 
-const sortByOptions = {
-    // save list of key value pairs to format API GET requests later on. 
-    // keys = human readable text
-    // values = api sortBy parameter values
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count',
-}
-
 class SearchBar extends React.Component {
     constructor(props){
         super(props);
@@ -21,6 +12,7 @@ class SearchBar extends React.Component {
             location:'',
             sortBy: 'best_match'
         };
+
         this.sortByOptions = {
             // save list of key value pairs to format API GET requests later on. 
             // keys = human readable text
@@ -34,8 +26,8 @@ class SearchBar extends React.Component {
         // dynamically display the list used to show the three sort options
         return (
             // more semantic to use singular form of the thing you are applying .map() to instead of key, val, all the way. Although harder to read without creating intermediate variables.
-            Object.keys(sortByOptions).map((sortByOption)=> {
-                const sortByOptionValue = sortByOptions[sortByOption]; // confusiongly, sortByOptionValue is used for the key of the list item. 
+            Object.keys(this.sortByOptions).map((sortByOption)=> {
+                const sortByOptionValue = this.sortByOptions[sortByOption]; // confusiongly, sortByOptionValue is used for the key of the list item. 
                 return <li key={sortByOptionValue}>{sortByOption}</li>
             })
         )
