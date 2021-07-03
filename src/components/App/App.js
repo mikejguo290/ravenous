c54e7a2a8e5b76dc8dfe9580163633a9c090808e
mikejguo290 <mikejguo290@gmail.com>
2021-07-03 18:53:51 +0100

    4.23 remove hard coded business and businesses data.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,21 +4,6 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 import Yelp from '../../util/Yelp';
 
-// App will be the component to first receive data from Yelp API - a list of business. simulate that by hard coding list of businesses here. 
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-
-const businesses = [business, business, business, business, business, business];
-
 class App extends React.Component {
   
   searchYelp(term, location, sortBy){
