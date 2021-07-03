49ef55c2c83e6d7454f96baecea1dcee63d28056
mikejguo290 <mikejguo290@gmail.com>
2021-07-03 20:59:38 +0100

    4.24 add constructor and set up state for businesses. bind searchYelp with 'this'.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,17 @@ import SearchBar from '../SearchBar/SearchBar';
 import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
-  
+  // having deleted mock data, use App to store result of calling Yelp api in this.state.businesses. 
+  // pass data as props
+  // modify searchYelp with Yelp from utils.
+  constructor(props){
+    super(props);
+    this.state = {
+      businesses:[]
+    }
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy){
     //function to simulate what clicking on "Let's go" button would do. - making a call to Yelp API.
     // will need to bind this when I use a constructor in App
@@ -16,7 +26,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar searchYelp={this.searchYelp.bind(this)}/>
+        <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={businesses} />
       </div>    
     )
