94835d49a6445e4a575ffbfe71a63327ad0310ca
mikejguo290 <mikejguo290@gmail.com>
2021-07-03 14:38:03 +0100

    4.11 format a get request url to Yelp's business search endpoint.

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,6 +2,7 @@
 const Yelp = {
     search (term, location, sortBy){
         /* return a promise which will resolve to list of businesses. */
-        return fetch();
+        const url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+        return fetch(url);
     }
 };
