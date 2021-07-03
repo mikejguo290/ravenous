473bd3b9ee661687d9b79d8680c5f5bb5711bc10
mikejguo290 <mikejguo290@gmail.com>
2021-07-03 18:47:25 +0100

    4.21 add key as special attribute to rendered list of businesses in BusinessList with business.id as value.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,13 +3,16 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 // render a list of returned Businesses from calling on YELP API
+/* each item in a React list component needs a unique key, 
+set Business components key to be business.id from json response. 
+key is defined only at the list component , not in the individual Business component level. */
 
 class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
                 {this.props.businesses.map((business)=>{
-                    return <Business business={business}/>;
+                    return <Business business={business} key={business.id}/>;
                 })}
             </div>
         )
