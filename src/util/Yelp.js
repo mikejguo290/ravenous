72cc816d569d05f403d60c2a3927db91c20594c6
mikejguo290 <mikejguo290@gmail.com>
2021-07-03 15:16:45 +0100

    comment on CORS policy.

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -6,3 +6,18 @@ const Yelp = {
         return fetch(url);
     }
 };
+
+/* CORS - Cross Origin Resource Sharing 
+    access of data and files across different origins. e.g. website and the api it calls. 
+    the browsers implement a Same Origin Resource policy which allows sites to request 
+    resources from server with the same origin but forbids cross origin resource sharing.
+    details: 
+    browser adds origin header to requests 
+    server adds Access-Control-Allow-Origin header to its response 
+    both headers needs to match
+    otherwise browser will prevent response data from being shared with its client. 
+
+    the solution is to FIRST, enable CORS on the server, SECOND, match url on the server to the client's url. 
+
+    e.g. the spotify Jamming project avoided this because there was a requirement 
+    to register app's url with spotify app account. */
