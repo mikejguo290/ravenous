a6d261aaa02d3e49416dd7cbfa0ec3d3bbe813b8
mikejguo290 <mikejguo290@gmail.com>
2021-07-03 13:51:00 +0100

    4.10 return a fetch() to return Promise that could resolve to list of businesses.

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,7 @@
 
-const Yelp = {};
+const Yelp = {
+    search (term, location, sortBy){
+        /* return a promise which will resolve to list of businesses. */
+        return fetch();
+    }
+};
