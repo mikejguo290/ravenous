0ac00cb88e2c4cd95b998edc5f557b77708e3b41
mikejguo290 <mikejguo290@gmail.com>
2021-07-05 12:10:16 +0100

    28.1 Make addresses clickable and have them open the address in Google Maps in a new tab.

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -5,6 +5,11 @@ import './Business.css' // import the css file which is on the same level as the
 
 class Business extends React.Component {
     render(){
+        const latitude = this.props.business.latitude;
+        const longitude = this.props.business.longitude;
+        // click on this formatted url to access location on google maps, in another tab
+        const locationURL = `https://www.google.com/maps?q=${latitude},${longitude}`;
+        const locationStyle = {textDecoration: 'none'}
         return (
             <div className="Business">
                 <div className="image-container">
@@ -12,11 +17,13 @@ class Business extends React.Component {
                 </div>
                 <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
-                    <div className="Business-address">
-                    <p>{this.props.business.address}</p>
-                    <p>{this.props.business.city}</p>
-                    <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
-                    </div>
+                    <a href={locationURL} target="_blank" rel="noreferrer noopener" style={locationStyle}>
+                        <div className="Business-address">
+                            <p>{this.props.business.address}</p>
+                            <p>{this.props.business.city}</p>
+                            <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
+                        </div>
+                    </a>
                     <div className="Business-reviews">
                     <h3>{this.props.business.category.toUpperCase()}</h3>
                     <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
